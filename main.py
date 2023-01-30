43c5a0e795efa688c9b2898773268c9b120b5f19
FlzGlz <51001772+richfls@users.noreply.github.com>
2023-01-30 09:02:09 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = []
+y = []
+
+#example 1
+# for i in range(-10,11,1):
+#     x.append(((-1*(2/5))*i)+3)
+
+# for i in range(-10,11,1):
+#     y.append(i)
+#example 2
+#for i in range(-10,11,1):
+    #x.append(np.absolute(i))
+
+#for i in range(-10,11,1):
+    #y.append(i)
+#example 3
+#for i in range(-10,11,1):
+    #x.append(-1*(i**2)+(2*i)+3)
+
+#for i in range(-10,11,1):
+    #y.append(i)
+#example 11
+#for i in range(-10,11,1):
+    #x.append(i**3)
+
+#for i in range(-10,11,1):
+    #y.append(i)
+#example 15
+#couldn't get it to work
+#for i in range(-10,11,1):
+    #x.append(1/np.cos(i))
+
+#for i in range(-10,11,1):
+    #y.append(i)
+plt.plot(y,x)
+plt.show()
